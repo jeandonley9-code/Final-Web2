3fd6610d6398c3534043ebe3632e50f55792656d
Jolibois Towensia <tjolibois@gmail.com>
2026-05-03 20:09:10 -0400

    modifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { AuthProvider } from './contexts/AuthContext'
 
 import Sidebar from './components/Sidebar'
 import Navbar from './components/Navbar'
